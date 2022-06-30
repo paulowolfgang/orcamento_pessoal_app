407e97ba46732e163a3bad39f8f58d99f2636071
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2022-06-30 12:21:01 -0300

    Criando o model e a migration da tabela de pagamentos

---
+++ database/migrations/2022_06_30_145007_create_payments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->date('date_payment')->nullable();
+            $table->date('date_due');
+            $table->float('value', 8, 2);
+            $table->boolean('status');
+            $table->timestamps();
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->unsignedBigInteger('id_categorie');
+            $table->foreign('id_categorie')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
